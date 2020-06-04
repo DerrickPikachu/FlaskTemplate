147ddb1320965979e72dce80772e02c84956c831
PikaChin <derrfgh4563@gmail.com>
2020-06-04 16:30:34 +0800

    Test global variable

--- flaskTest.py
+++ flaskTest.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+counter = 0
+
 
 @app.route('/')
 def index():
-    return "Hello World"
+    global counter
+    counter += 1
+    return str(counter)
 
 
 if __name__ == "__main__":
